4cf13770b03860c4f9dc8d8c4cc4426e13aba958
Rodrigo Raniero <rdgraniero@hotmail.com>
2026-04-10 08:19:16 -0300

    temporarily disable login

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,18 @@
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+export function createClient() {
+  const supabaseUrl =
+    process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+  const supabaseKey =
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ||
+    process.env.SUPABASE_ANON_KEY ||
+    process.env.SUPABASE_PUBLISHABLE_KEY;
+
+  if (!supabaseUrl || !supabaseKey) {
+    throw new Error("Supabase env ausente.");
+  }
+
+  return createBrowserClient(supabaseUrl, supabaseKey);
+}
